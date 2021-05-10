72232e31e14585cea3a33b59d89f0d699feacaec
Егор Воронянский <voronyansky.egor@yandex.ru>
2021-05-10 18:37:18 +0800

    Моя первая задача

--- chapter1/src/main/java/ru/skillfactory/tasks/ContainsOnlyDigits.java
+++ chapter1/src/main/java/ru/skillfactory/tasks/ContainsOnlyDigits.java
@@ -9,6 +9,11 @@ public class ContainsOnlyDigits {
      * @return - true, если в строчке только цифры, в любом другом случае false
      */
     public boolean containsOnlyDigits(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
         return true;
     }
 }
